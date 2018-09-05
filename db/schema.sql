f66a2b9510c0d260e3c0ea9ae69bc186aa8b229a
Vidhi <panchalvidhi@ymail.com>
2018-09-05 15:38:34 -0400

    adding files

---
+++ db/schema.sql
@@ -0,0 +1,11 @@
+CREATE DATABASE burgers_db;
+
+USE burgers_db;
+
+CREATE TABLE burgers(
+id INT NOT NULL AUTO_INCREMENT,
+burger_name VARCHAR(100),
+devoured BOOLEAN NOT NULL DEFAULT false,
+date TIMESTAMP NOT NULL,
+PRIMARY KEY(id)
+);
---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+USE burgers_db;
+
+
+INSERT INTO burgers (burger_name) VALUES ("Grilled Chicken Burger");
+INSERT INTO burgers (burger_name) VALUES ("Turkey Bacon Ranch Burger");
+INSERT INTO burgers (burger_name) VALUES ("Egg and Sausage Burger");
